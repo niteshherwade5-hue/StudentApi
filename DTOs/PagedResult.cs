bd1137d7f77ca910e73b7ba50e02067b73dfaa7f
Nitesh Herwade <niteshherwade5@gmail.com>
2026-01-08 21:46:54 +0100

    Trigger workflow

--- DTOs/PagedResult.cs
+++ DTOs/PagedResult.cs
@@ -2,6 +2,6 @@ public class PagedResult<T>
 {
     public List<T> Items { get; set; } = new();
     public int TotalCount { get; set; }
-    public int Page { get; set; }
+    public int Page { get; set; } //Page
     public int PageSize { get; set; }
 }
