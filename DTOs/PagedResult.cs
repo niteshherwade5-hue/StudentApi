bbea3e546159cb1d8f1cc09b7d679f51ffb22dd4
Nitesh Herwade <niteshherwade5@gmail.com>
2026-01-08 22:03:56 +0100

    Trigger deployment after IIS config permissions

--- DTOs/PagedResult.cs
+++ DTOs/PagedResult.cs
@@ -1,7 +1,7 @@
 public class PagedResult<T>
 {
     public List<T> Items { get; set; } = new();
-    public int TotalCount { get; set; }
+    public int TotalCount { get; set; } //Totalcount
     public int Page { get; set; } //Page
     public int PageSize { get; set; } //pagesize
 }
