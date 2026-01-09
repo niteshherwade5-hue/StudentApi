c1fa044dc4b567e27b86c2fe97d614a98c8f042f
Nitesh Herwade <niteshherwade5@gmail.com>
2026-01-09 18:47:35 +0100

    Recreate publish folder

--- DTOs/UserRegisterDto.cs
+++ DTOs/UserRegisterDto.cs
@@ -2,7 +2,7 @@ namespace WebApplication1.DTOs
 {
     public class UserRegisterDto
     {
-        public string Username { get; set; } = default!;
+        public string Username { get; set; } = default!;//UserNmae
         public string Password { get; set; } = default!; //Pass
         public string Role { get; set; } = "User";  // default role
     }
