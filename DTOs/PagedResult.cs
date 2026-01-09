8dddcf5f8120c7916a9c6e896938e0baa36d7c0a
Nitesh Herwade <niteshherwade5@gmail.com>
2026-01-09 17:54:40 +0100

    Trigger deployment after fixing runner account

--- DTOs/PagedResult.cs
+++ DTOs/PagedResult.cs
@@ -1,6 +1,6 @@
 public class PagedResult<T>
 {
-    public List<T> Items { get; set; } = new();
+    public List<T> Items { get; set; } = new(); //Items
     public int TotalCount { get; set; } //Totalcount123
     public int Page { get; set; } //Page
     public int PageSize { get; set; } //pagesize123
--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae081d5ffad6eb628c1e34a5e2a94e79c236b1cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
